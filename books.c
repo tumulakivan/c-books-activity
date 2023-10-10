bce2ccaac31cdbec2d6dbccd38bdacbb37437793
tumulakivan <62394230+tumulakivan@users.noreply.github.com>
2023-10-10 09:25:59 +0800

    Add files via upload

--- books.c
+++ books.c
@@ -1,80 +1,70 @@
+#include "books.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "books.h"
 
-int searchAuthor(char author[], Book books[], int count)
-{
-	int i;
+int searchAuthor(char author[], Book books[], int count) {
+  int i, ctr = 0;
 
-	for (i = 0; i < count; i++)
-	{
-		if (strcmp(books[i].author, author) == 0)
-			return 1;
-	}
+  for (i = 0; i < count; i++) {
+    if (strcmp(books[i].author, author) == 0)
+      ctr++;
+  }
 
-	return 0;
+  return ctr++;
 }
 
-int searchBook(char title[], Book books[], int count)
-{
-	int i, num = 0;
+int searchBook(char title[], Book books[], int count) {
+  int i, num = 0;
 
-	for (i = 0; i < count; i++)
-	{
-		if (strcmp(books[i].title, title) == 0)
-			num++;
-	}
+  for (i = 0; i < count; i++) {
+    if (strcmp(books[i].title, title) == 0)
+      num++;
+  }
 
-	return num;
+  return num;
 }
 
-int compareDates(const void *a, const void *b)
-{
-	const Book *date1 = (const Book *)a;
-	const Book *date2 = (const Book *)b;
+int compareDates(const void *a, const void *b) {
+  const Book *date1 = (const Book *)a;
+  const Book *date2 = (const Book *)b;
 
-	return strcmp(date1->datePublished, date2->datePublished);
+  return strcmp(date1->datePublished, date2->datePublished);
 }
 
-void addBook(Book books[], int *count, Book newBook)
-{
+void addBook(Book books[], int *count, Book newBook) {
 
-	if (strcmp(books[*count].id, newBook.id) == 1)
-		printf("Book already exists.\n");
-	else
-		books[*count] = newBook;
+  if (strcmp(books[*count].id, newBook.id) == 1)
+    printf("Book already exists.\n");
+  else
+    books[*count] = newBook;
 
-	(*count)++;
+  (*count)++;
 }
 
-void display(Book books[], int count)
-{
-	int i;
+void display(Book books[], int count) {
+  int i;
 
-	for (i = 0; i < count; i++)
-	{
-		printf("\n");
-		printf("Book id: %s\n", books[i].id);
-		printf("Book title: %s\n", books[i].title);
-		printf("Book author: %s\n", books[i].author);
-		printf("Book publishing date: %s\n", books[i].datePublished);
-		printf("Book number of copies: %d\n\n", books[i].copies);
-	}
+  for (i = 0; i < count; i++) {
+    printf("\n");
+    printf("Book id: %s\n", books[i].id);
+    printf("Book title: %s\n", books[i].title);
+    printf("Book author: %s\n", books[i].author);
+    printf("Book publishing date: %s\n", books[i].datePublished);
+    printf("Book number of copies: %d\n\n", books[i].copies);
+  }
 }
 
-void displayBooksByYear(Book books[], int count)
-{
-	int i;
+void displayBooksByYear(Book books[], int count) {
+  int i;
 
-	qsort(books, count, sizeof(Book), compareDates);
-	for (i = 0; i < count; i++)
-	{
-		printf("\n");
-		printf("Book id: %s\n", books[i].id);
-		printf("Book title: %s\n", books[i].title);
-		printf("Book author: %s\n", books[i].author);
-		printf("Book publishing date: %s\n", books[i].datePublished);
-		printf("Book number of copies: %d\n\n", books[i].copies);
-	}
+  qsort(books, count, sizeof(Book), compareDates);
+  for (i = 0; i < count; i++) {
+    printf("\n");
+    printf("Book id: %s\n", books[i].id);
+    printf("Book title: %s\n", books[i].title);
+    printf("Book author: %s\n", books[i].author);
+    printf("Book publishing date: %s\n", books[i].datePublished);
+    printf("Book number of copies: %d\n\n", books[i].copies);
+  }
 }
--- main.c
+++ main.c
@@ -1,70 +1,68 @@
+#include "books.h"
 #include <stdio.h>
 #include <string.h>
-#include "books.h"
 
-int main()
-{
-	Book books[10];
-	Book newBook; // in case a new book is to be added to the array
-	int count = 0, cp, choice;
-	char id[5], t[100], auth[50], dP[12];
+int main() {
+  Book books[10];
+  Book newBook; // in case a new book is to be added to the array
+  int count = 0, cp, choice;
+  char id[5], t[100], auth[50], dP[12];
 
-	printf("Hello, welcome to library!\n");
-	while (1)
-	{
-		printf("\nWhat would you like to do?\n");
-		printf("1.) Add a book\n");
-		printf("2.) Search for a book with a specific title\n");
-		printf("3.) Search for a book from a specific author\n");
-		printf("4.) Display all books by publishing year\n");
+  printf("Hello, welcome to library!\n");
+  while (1) {
+    printf("\nWhat would you like to do?\n");
+    printf("1.) Add a book\n");
+    printf("2.) Search for a book with a specific title\n");
+    printf("3.) Search for a book from a specific author\n");
+    printf("4.) Display all books by publishing year\n");
 
-		scanf("%d", &choice);
-		switch (choice)
-		{
-		case 0:
-			printf("\nProgram terminated.\n");
-			return 1;
-		case 1:
-			printf("\nEnter book id: ");
-			scanf(" %[^\n]s", &newBook.id);
-			printf("Enter book title: ");
-			scanf(" %[^\n]s", &newBook.title);
-			printf("Enter book author: ");
-			scanf(" %[^\n]s", &newBook.author);
-			printf("Enter date published: ");
-			scanf(" %[^\n]s", &newBook.datePublished);
-			printf("Enter number of copies: ");
-			scanf("%d", &newBook.copies);
-			addBook(books, &count, newBook);
-			if (count > 0)
-				printf("Added book successfully!\n");
-			printf("%s\n", newBook.title);
-			break;
-		case 2:
-			printf("\nEnter title search: ");
-			scanf(" %[^\n]s", t);
-			if (searchBook(t, books, count) == 1)
-				printf("Book found!\n");
-			else
-				printf("Book not found.\n");
-			break;
-		case 3:
-			printf("\nEnter author search: ");
-			scanf(" %[^\n]s", auth);
-			if (searchAuthor(auth, books, count) > 0)
-				printf("%d book/s by %s found!\n", searchAuthor(auth, books, count), auth);
-			else
-				printf("No entries by %s.\n", auth);
-			break;
-		case 4:
-			printf("\nBooks sorted by publishing year.\n");
-			displayBooksByYear(books, count);
-			break;
-		default:
-			printf("\nInvalid option.\n");
-			break;
-		}
-	}
+    scanf("%d", &choice);
+    switch (choice) {
+    case 0:
+      printf("\nProgram terminated.\n");
+      return 1;
+    case 1:
+      printf("\nEnter book id: ");
+      scanf(" %[^\n]s", &newBook.id);
+      printf("Enter book title: ");
+      scanf(" %[^\n]s", &newBook.title);
+      printf("Enter book author: ");
+      scanf(" %[^\n]s", &newBook.author);
+      printf("Enter date published: ");
+      scanf(" %[^\n]s", &newBook.datePublished);
+      printf("Enter number of copies: ");
+      scanf("%d", &newBook.copies);
+      addBook(books, &count, newBook);
+      if (count > 0)
+        printf("Added book successfully!\n");
+      printf("%s\n", newBook.title);
+      break;
+    case 2:
+      printf("\nEnter title search: ");
+      scanf(" %[^\n]s", t);
+      if (searchBook(t, books, count) == 1)
+        printf("Book found!\n");
+      else
+        printf("Book not found.\n");
+      break;
+    case 3:
+      printf("\nEnter author search: ");
+      scanf(" %[^\n]s", auth);
+      if (searchAuthor(auth, books, count) > 0)
+        printf("%d book/s by %s found!\n", searchAuthor(auth, books, count),
+               auth);
+      else
+        printf("No entries by %s.\n", auth);
+      break;
+    case 4:
+      printf("\nBooks sorted by publishing year.\n");
+      displayBooksByYear(books, count);
+      break;
+    default:
+      printf("\nInvalid option.\n");
+      break;
+    }
+  }
 
-	return 0;
+  return 0;
 }
